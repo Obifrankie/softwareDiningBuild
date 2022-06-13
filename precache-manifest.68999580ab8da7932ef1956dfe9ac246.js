aacc5c6bc2b1e7d5f0be204708dc18fd47b7c505
Obifrankie <obifranklina@gmal.com>
2022-06-13 17:47:33 +0100

    new commit

---
+++ precache-manifest.68999580ab8da7932ef1956dfe9ac246.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56e8f297f8da0960486b17971b5f4a36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8771644a23ba204a2af5",
+    "url": "/static/css/main.9516a8fc.chunk.css"
+  },
+  {
+    "revision": "518e51f39853dc526471",
+    "url": "/static/js/2.3657a948.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/static/js/2.3657a948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8771644a23ba204a2af5",
+    "url": "/static/js/main.52fab5d8.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.52fab5d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "0100cb06e8be3561048739ba5a5ea0fa",
+    "url": "/static/media/memoriesText.0100cb06.png"
+  }
+]);
